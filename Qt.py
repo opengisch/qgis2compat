2dbc4d746a1442554dbc2a4afd3196d14d8000fd
Marco Bernasocchi <marco@opengis.ch>
2016-09-14 18:01:21 +0200

    fixed init to suit QGIS

---
+++ Qt.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+    QtCore.py
+    ---------------------
+    Date                 : November 2015
+    Copyright            : (C) 2015 by Matthias Kuhn
+    Email                : matthias at opengis dot ch
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+__author__ = 'Marco Bernasocchi'
+__date__ = 'September 2016'
+__copyright__ = '(C) 2016, Marco Bernasocchi'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
+
+from PyQt4.Qt import *
--- __init__.py
+++ __init__.py
@@ -1,24 +1,35 @@
 # -*- coding: utf-8 -*-
-
 """
-***************************************************************************
-    __init__.py
-    ---------------------
-    Date                 : November 2015
-    Copyright            : (C) 2015 by Matthias Kuhn
-    Email                : matthias at opengis dot ch
-***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************
+/***************************************************************************
+ QGIScompat
+           A PyQt5 compatibility layer for QGIS < 2.14
+
+                              -------------------
+        begin                : 2014-07-28
+        git sha              : $Format:%H$
+        copyright            : (C) 2016 OPENGIS.ch
+        email                : info@opengis.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 """
 
-__author__ = 'Matthias Kuhn'
-__date__ = 'November 2015'
-__copyright__ = '(C) 2015, Matthias Kuhn'
-# This will get replaced with a git SHA1 when you do a git archive
-__revision__ = '$Format:%H$'
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    return QgisCompat()
+
+
+class QgisCompat(object):
+    def initGui(self):
+        pass
+
+    def unload(self):
+        pass
