1864a3a4218457de10acb193c9bca4eb269d4d1c
Matthias Kuhn <matthias@opengis.ch>
2016-12-05 09:25:51 +0100

    Add QgsMapLayerProxyModel alias in qgis.core

--- apicompat/__init__.py
+++ apicompat/__init__.py
@@ -16,3 +16,4 @@ if not initialized and hasattr(qgis.utils, 'QGis'):
     import qgis2compat.apicompat.qgsmapthemecollection
     import qgis2compat.apicompat.qgsproject
     import qgis2compat.apicompat.qgsdatasourceuri
+    import qgis2compat.apicompat.qgsmaplayerproxymodel
