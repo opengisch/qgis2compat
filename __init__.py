7bbfc399eb77f9f9402b50a7e2281f0a52aca83c
Marco Bernasocchi <marco@opengis.ch>
2016-12-13 14:37:08 +0100

    added suport for QGIS enterprise

--- __init__.py
+++ __init__.py
@@ -23,20 +23,33 @@
 import sys
 import qgis.utils
 
-
+QGIS_VERSION = 0
 
 def log(message):
     print('QGIS2compat: %s' % message)
 
 
+def _qgis2_version():
+    #QGIS enterprise uses a different versioning schema.
+    # Let's remap their schema to the real API version
+    qgis_enterprise_map = {21502: 20800}
+
+    from qgis.PyQt.QtCore import QCoreApplication
+    version = qgis.utils.QGis.QGIS_VERSION_INT
+    if QCoreApplication.applicationName().startswith('QGIS Enterprise'):
+        version = qgis_enterprise_map[qgis.utils.QGIS_VERSION_INT]
+    return version
+
+
 # qgis.utils.QGis is available in QGIS < 3
 if hasattr(qgis.utils, 'QGis'):
-    QGIS_VERSION = qgis.utils.QGis.QGIS_VERSION_INT
     import qgis2compat.PyQt
+    QGIS_VERSION = _qgis2_version()
 
     log('setting qgis.PyQt = qgis2compat.PyQt')
     sys.modules["qgis.PyQt"] = qgis2compat.PyQt
-
+else:
+    QGIS_VERSION = qgis.core.Qgis.QGIS_VERSION_INT
 
 
 # FROM here is all needed to make QGIS's plugin manager happy
